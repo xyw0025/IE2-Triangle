9f42e009983b6b7152316ac9576e4ad705190384
wen <xyw0025@gmail.com>
2023-10-25 01:05:24 -0400

    chore: just pdf and test files

--- statement_coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Change to the directory containing the Python script
-cd "./test_suit"
-coverage run -m pytest test_statementCoverage.py
-coverage report -m --include="test_statementCoverage.py"
-coverage html -d statement_html
